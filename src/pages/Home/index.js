17245321e4729007fc9a2dd7028daa76422d2aa0
Anthony Spault <anthony@spault.fr>
2018-12-20 12:24:38 +0100

    WIP home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,8 +3,21 @@ import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 
 import RegularLayout from '../../layouts/RegularLayout'
+import Movie from '../../components/movies/movie'
+
 
 import { getMovies } from '../../redux/actions/movie'
+
+import {
+    Nominees,
+    TitleSection,
+    Month,
+    CTAvote,
+    CTAnominees,
+    About,
+    AboutSubtitle,
+    AboutContent
+} from './style'
 
 class Home extends Component {
     
@@ -20,17 +33,44 @@ class Home extends Component {
     }
 
     render() {
+        const monthsNames = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"]
+        const currentDate = new Date()
+
         return (
             <RegularLayout>
-                <h1>Hello</h1>
-                { this.props.movies.data && this.props.movies.data.map(movie => (
-                    <div key={movie._id}>
-                        <img src={movie.poster} alt="poster" />
-                        <p>{movie.title}</p>
-                        <p>{movie.directors}</p>
-                        <Link to={`/movie/${movie._id}`}>En savoir plus</Link>
-                    </div>
-                ))}
+                {/* TODO : GET SELECTIONNED MOVIE FROM THE PREVIOUS MONTH */}
+                <Nominees>
+                    <TitleSection>
+                        <Month>                                
+                            <span>{monthsNames[currentDate.getMonth()]}&nbsp;{currentDate.getFullYear()}</span>
+                            <span>Nominés du mois</span>
+                        </Month>
+                        <CTAvote>
+                            Votez maintenant
+                        </CTAvote>
+                    </TitleSection>
+                    { this.props.movies.data && this.props.movies.data.map(movie => (
+                        <Movie movie={movie}></Movie>
+                    ))}
+                    <CTAnominees>
+                        Découvrez les nominés
+                    </CTAnominees>
+                </Nominees>
+
+                <About>
+                    <AboutSubtitle>
+                        Prix brutus c'est
+                    </AboutSubtitle>
+                    <AboutContent>
+                        la sélection du meilleur film de 
+                        l’année selon le vote du public. 
+                        Notre objectif est de valoriser 
+                        et d’apporter un poids à votre 
+                        voix. Les césars n’ont qu’à bien 
+                        se tenir : ici, vous êtes le jury.
+                    </AboutContent>
+                    <i className="fas fa-chevron-down"></i>
+                </About>
             </RegularLayout>
         )
     }
